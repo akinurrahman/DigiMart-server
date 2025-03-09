a803241a6db6257050685bbe04451ea682b32471
Akinur Rahman <akinurrahmanofficial@gmail.com>
2025-03-09 16:26:30 +0530

    added cloudinary uploader and more

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -6,13 +6,17 @@ dotenv.config({
 
 export const envConfig = {
   port: process.env.PORT || 8000,
-  node_env : process.env.NODE_ENV || 'production',
-  cors_origin : process.env.CORS_ORIGIN,
+  node_env: process.env.NODE_ENV || "production",
+  cors_origin: process.env.CORS_ORIGIN,
   mongodb_uri: process.env.MONGODB_URI,
-  accesstoken: process.env.ACCESS_TOKEN,
+
   accesstoken_secret: process.env.ACCESS_TOKEN_SECRET,
   accesstoken_expiry: process.env.ACCESS_TOKEN_EXPIRY,
-  refreshtoken: process.env.REFRESH_TOEKN,
+
   refreshtoken_secret: process.env.REFRESH_TOKEN_SECRET,
   refreshtoken_expiry: process.env.REFRESH_TOKEN_EXPIRY,
+
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 };
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+import { envConfig } from "../config/env.config";
+
+cloudinary.config({
+  cloud_name: envConfig.cloudinary_cloud_name,
+  api_key: envConfig.cloudinary_api_key,
+  api_secret: envConfig.cloudinary_api_secret,
+});
+
+export const uploadToCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    fs.unlinkSync(localFilePath); //Delete local file after upload
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // removed failed upload file
+    return null;
+  }
+};
